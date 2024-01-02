12965bad5ea525f630f780f716bbe6be69d72584
ilian-bonsens <ilian.bonsens@laplateforme.io>
2024-01-02 10:21:12 +0100

    job 1 et 2

---
+++ jour01/job02.py
@@ -0,0 +1,10 @@
+class Operation:
+    def __init__ (self, nombre1=12, nombre2=3):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+    def afficher(self):
+        print(f"Le nombre 1 est : {self.nombre1}\nLe nombre 2 est : {self.nombre2}")
+
+op = Operation()
+op.afficher()
